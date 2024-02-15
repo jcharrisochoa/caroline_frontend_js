e694c6cdd7e7a0c60be358f785ff9caf5e05ec60
Oficina <jcharrisochoa@gmail.com>
2024-02-15 18:40:40 -0500

    Implementando Router

---
+++ src/routes/routes.js
@@ -0,0 +1,25 @@
+export const PATHS = {
+    login: {
+        path: '/',
+        template: '<h1>Login</h1>'
+    },
+    layout:{
+        path: '/layout',
+        template: './src/module/layout/index.html'
+    },
+    concat:{
+        path: '/',
+        template: './src/module/layout/index.html'
+    },
+    error:{
+        path: '/',
+        template: '<h1>Error</h1>'
+    },
+    /*layout: {
+        template: './src/module/layout/index.html',
+        callback: function() {
+           
+        }
+    }*/
+
+};
